e9d40c64b0bf97e1adae75d2e6d9b697bad4dd85
Charlie Rudenstål <charlie4@gmail.com>
2015-08-29 18:05:00 +0200

    Add mock tasks and basic GET, POST and DELETE methods

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,55 @@
-from flask import Flask
+import sys
+import shortuuid
+from flask import Flask, jsonify, request, abort
 app = Flask(__name__)
 
-@app.route('/')
+tasks = {
+    shortuuid.uuid(): { 'text': 'Discuss report with John', 'done': True },
+    shortuuid.uuid(): { 'text': 'Get a haircut', 'done': True },
+    shortuuid.uuid(): { 'text': 'Pay electricity bill', 'done': True },
+    shortuuid.uuid(): { 'text': 'Check gym hours', 'done': True },
+}
+
+@app.route('/tasks', methods=['GET'])
 def index():
-    return 'Hello World!'
+    return jsonify({'tasks': tasks})
+
+@app.route('/tasks/<id>', methods=['GET'])
+def get(id):
+    if id not in tasks:
+        abort(404)
+    return jsonify(tasks[id])
+
+@app.route('/tasks', methods=['POST'])
+def create():
+    input = request.get_json()
+
+    # Request body must contain a text attribute
+    if input is None or not 'text' in input:
+        abort(400)
+
+    # Build the task
+    id = shortuuid.uuid()
+    task = {
+        'text': input.get('text'),
+        'done': input.get('done'),
+    }
+
+    # Store it
+    tasks[id] = task;
+
+    # Create a response
+    resp = jsonify(task)
+    resp.status_code = 201
+    resp.headers['location'] = '/tasks/%s' % id
+    return resp
+
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete(id):
+    if id not in tasks:
+        abort(404)
+    del tasks[id]
+    return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
