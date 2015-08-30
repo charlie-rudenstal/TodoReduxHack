6529cbbddfe876416c24fedbf0e26b78cac76ed3
Charlie Rudenstål <charlie4@gmail.com>
2015-08-30 20:44:13 +0200

    Add support for Less
    Will also rename the "client" folder into "js" to be consistent with less

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,23 +1,38 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var less = require('gulp-less');
 var spawn = require('child_process').spawn;
 var node;
 
+/**
+ * Build
+ */
+gulp.task('js', function() {
+	return gulp.src('src/js/**/*.js')
+		.pipe(babel())
+		.pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('less', function() {
+	return gulp.src('src/less/**/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['js', 'less']);
+
+/**
+ * Server and Watch
+ */
 gulp.task('server', function() {
 	if (node) {
 		node.kill();
 	}
 	node = spawn('node', ['server.js'], {stdio: 'inherit'});
-})
-
-gulp.task('build', function() {
-	return gulp.src('src/**/*.js')
-		.pipe(babel())
-		.pipe(gulp.dest('dist'))
 });
 
 gulp.task('watch', ['build', 'server'], function() {
 	gulp.watch(['src/**/*.js', './server.js'], ['build', 'server'])
-})
+});
 
 gulp.task('default', ['build', 'server']);
--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use('/js', express.static(__dirname + '/dist/client'));
+app.use('/js', express.static(__dirname + '/dist/js'));
+app.use('/css', express.static(__dirname + '/dist/css'));
 
 var port = process.env.PORT || 5001;
 app.listen(port);
