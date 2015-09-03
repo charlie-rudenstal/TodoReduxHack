e9808ec63aca28c72bfe2958b6f3138950f6b74a
Charlie Rudenstål <charlie4@gmail.com>
2015-09-03 21:10:49 +0200

    Make it possible to mark tasks as done/undone
    Make tasks immutable using Immutable.js.  Change the data structure of tasks, to id => task, to facilitate updates,  uniqueness and optimistic updates.

--- frontend/src/js/components/TaskItem.js
+++ frontend/src/js/components/TaskItem.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { autobind } from 'core-decorators';
 
 export default class TaskItem extends React.Component {
 
+	@autobind
+	onChangeDone() {
+		const done = React.findDOMNode(this.refs.checkDone).checked;
+		if (this.props.onUpdateTask) {
+			this.props.onUpdateTask(this.props.task.set('done', done));
+		}
+	}
+
 	render() {
 		return  (
-			<div style={{ color: this.props.task.done ? 'gray' : 'black' }}>
-				{this.props.task.text}
+			<div>
+				<input
+					type="checkbox"
+					ref='checkDone'
+					checked={this.props.task.get('done')}
+					onChange={this.onChangeDone} />
+				{this.props.task.get('text')}
 			</div>
 		);
 	}
--- frontend/src/js/store.js
+++ frontend/src/js/store.js
@@ -2,12 +2,26 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import createLogger from 'redux-logger';
 import tasks from './reducers/tasks';
+import Immutable from 'immutable';
 
 const reducers = combineReducers({
 	tasks,
 });
 
-const loggerMiddleware = createLogger();
+const loggerMiddleware = createLogger({
+	// transform immutable objects into JSON
+	// (https://github.com/fcomb/redux-logger)
+	transformer: (state) => {
+		for (var i of Object.keys(state)) {
+			if (Immutable.Iterable.isIterable(state[i])) {
+				return state[i].toJS();
+			} else {
+				return state[i];
+			}
+		}
+	}
+});
+
 export default applyMiddleware(
 	promiseMiddleware,
 	loggerMiddleware)
